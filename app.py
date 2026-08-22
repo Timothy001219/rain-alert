6fa58e36b17e28e878d93082dbeeda4748fcbb6d
Timothy001219 <timothy1219@gmail.com>
2026-08-22 20:36:13 +0800

    Integrate Flask web service with weather monitor
    Add Flask web service to run alongside weather monitoring.

---
+++ app.py
@@ -0,0 +1,23 @@
+from threading import Thread
+from flask import Flask
+import main  # 匯入原本的輪詢主程式
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return 'Weather Monitor is Running!'
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+# 在背景啟動 Flask Web 服務，同時執行天氣監控迴圈
+if __name__ == '__main__':
+    t = Thread(target=run)
+    t.start()
+
+    # 執行你原本的輪詢程式
+    main.main()
